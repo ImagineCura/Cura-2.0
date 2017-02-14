c009aebdb05e96b75e2072feb9d28f6fc888e9a2
Rakshit Bhaskar <Rakshit@Rakshits-MacBook-Air.local>
2017-02-14 22:30:26 +1300

    Initial Commit

---
+++ Cura 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cura 2.0
+//
+//  Created by Rakshit Bhaskar on 14/02/17.
+//  Copyright © 2017 Caregiver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
